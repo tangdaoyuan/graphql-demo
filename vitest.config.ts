ec556996ef78d5fc53d64e50c01d29ee392b6d81
tedy <1197633750@qq.com>
2022-08-22 18:15:22 +0800

    chore: shrink test range

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  test: {
+    include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
