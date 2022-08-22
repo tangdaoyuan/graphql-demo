1fdd58bb1508a0f5f9e3db98cb30241a523e0870
tedy <1197633750@qq.com>
2022-08-22 18:12:43 +0800

    chore: lint fixed

--- client-vue2/src/router/index.ts
+++ client-vue2/src/router/index.ts
@@ -11,7 +11,7 @@ const router = new VueRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/about',
@@ -19,9 +19,9 @@ const router = new VueRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
 })
 
 export default router
